cfe9b2cfcd2fc5cb3fb790874bf19a88e00068cb
Ayi <ayianjani774@gmail.com>
2022-03-22 08:09:42 +0700

    menambah data baru pada userbaruseeder

--- database/seeders/UserBaruSeeder.php
+++ database/seeders/UserBaruSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserBaruSeeder extends Seeder
 {
@@ -14,6 +15,14 @@ class UserBaruSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data=[
+            [
+                'username' => 'admin',
+                    'name' => 'Administrator Baru',
+                    'email' => 'admin@admin.com',
+                    'password' => Hash::make('password')
+            ]
+        ];
+        \DB::table('users')->insert($data);
     }
 }
